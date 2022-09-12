c34c977a00ae5dee26e489d62bf5528f0c2f42ad
Yiran Zhu <83456188+IsleZhu@users.noreply.github.com>
2022-09-12 14:40:21 -0400

    Update Sym.py

--- Sym.py
+++ Sym.py
@@ -1,16 +1,22 @@
 import math
 
+
 class Sym:
+    
     def __init__(self,col=0,name=""):
         self.n = 0
         self.at = col
         self.name = name
         self.has = {} # Save values in a dictionary {value: number}
+        
+        
 
     def add(self, v):
+        
         """
         Add symbolic value v to given column. If dictionary has contains key v, add 1 to its value; otherwise add key v to has with value 1.
         """
+        
         if v != "?":
             self.n = self.n + 1
             if v in self.has:
@@ -18,21 +24,29 @@ class Sym:
             else:
                 self.has[v] = 1
 
+                
+                
     def mid(self):
+        
         """
         Return the most common value in this symbolic column.
         """
+        
         most = -1
         for key in self.has:
             if self.has[key] > most:
                 most = self.has[key]
                 mode = key
         return mode
+    
+    
 
     def div(self):
+        
         """
         Calculate entropy of the symbolic column.
         """
+        
         entropy = 0
         for key in self.has:
             if self.n != 0:
