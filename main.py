555273fc6e033edb12e63ed7923648aa65cd99ed
yzhu27 <yhzhu27@gmail.com>
2022-09-18 18:41:29 -0400

    add TODO

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from tests import test_engine
 
 
 if __name__ == '__main__':
+    #TODO: Here to initial the with cli function (in utils), make user can modify the elements with cli
     test_engine.runs("ALL")
     exit(test_engine.fails)
